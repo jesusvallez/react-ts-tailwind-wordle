31424942a5223e57787ea8d18e9cbeac2612fa2d
Jesus Vallez Gomis <jesusvg91@gmail.com>
2022-05-02 15:34:32 +0200

    chore(config): added confetti and svgr pluging

---
+++ src/assets/index.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as BackspaceLogo } from './backspace.svg'
+import { ReactComponent as EnterLogo } from './enter.svg'
+
+export { BackspaceLogo, EnterLogo }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
 })

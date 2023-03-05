e80080f3f58f061cfb6756b2f1989d075d17c210
Jesus Vallez Gomis <jesusvg91@gmail.com>
2023-03-05 12:56:45 +0100

    chore(linter): prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   ...require('eslint-config-standard-typescript-prettier/prettier'),
   semi: false, // This is how you turn off semicolons, by the way
+  trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'always',
+  semi: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
 }
